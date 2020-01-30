b24fa474f641c2b1beb1d866fbef7ae2ed75fbef
tiger52 <tiger_s@meta.ua>
2020-01-30 10:22:25 +0200

    added helpers for AWS

---
+++ python/aws/delete_bucket.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+This script delete all versions of all files and after that delete bucket
+It should use when bucket has VERSIONING
+"""
+import boto3
+
+def del_bucket(BUCKET):
+    print("=== {} ===".format(BUCKET))
+    print("Removing all versions from {}".format(BUCKET))
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET)
+    bucket.object_versions.delete()
+    print("versions deleted")
+    # if you want to delete the now-empty bucket as well, uncomment this line:
+    print("start delete bucket".format(BUCKET))
+    bucket.delete()
+    print("bucket has been deleted")
+
+
+bucket_list_decaf=[
+    "hcom-kasset-milan-cmtc-content-us-west-2",
+    "hcom-kasset-milan-cobranded-content-us-west-2",
+]
+
+for bucket in bucket_list_decaf:
+    del_bucket(bucket)
